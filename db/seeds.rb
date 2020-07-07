417630cc00f1efc34b6f8fc1f9113b6d6ecaaba3
Andrew Trac <andrewtrac@Andrews-Air.Home>
2020-07-07 18:21:28 -0400

    new Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -162,4 +162,265 @@ test_row22 = WalledNode.create(
   col: 45,
 )
 
+face_row = WalledNode.create(
+  name: "test",
+  row: 0,
+  col: 18,
+)
+face_row1 = WalledNode.create(
+  name: "test",
+  row: 0,
+  col: 19,
+)
+face_row2 = WalledNode.create(
+  name: "test",
+  row: 0,
+  col: 20,
+)
+face_row3 = WalledNode.create(
+  name: "test",
+  row: 0,
+  col: 21,
+)
+face_row4 = WalledNode.create(
+  name: "test",
+  row: 0,
+  col: 22,
+)
+face_row5 = WalledNode.create(
+  name: "test",
+  row: 0,
+  col: 23,
+)
+face_row6 = WalledNode.create(
+  name: "test",
+  row: 0,
+  col: 24,
+)
+face_row7 = WalledNode.create(
+  name: "test",
+  row: 0,
+  col: 25,
+)
+face_row8 = WalledNode.create(
+  name: "test",
+  row: 0,
+  col: 26,
+)
+face_row9 = WalledNode.create(
+  name: "test",
+  row: 1,
+  col: 17,
+)
+face_row10 = WalledNode.create(
+  name: "test",
+  row: 1,
+  col: 27,
+)
+face_row11 = WalledNode.create(
+  name: "test",
+  row: 2,
+  col: 16,
+)
+face_row12 = WalledNode.create(
+  name: "test",
+  row: 2,
+  col: 28,
+)
+face_row13 = WalledNode.create(
+  name: "test",
+  row: 3,
+  col: 15,
+)
+face_row14 = WalledNode.create(
+  name: "test",
+  row: 3,
+  col: 29,
+)
+face_row15 = WalledNode.create(
+  name: "test",
+  row: 4,
+  col: 14,
+)
+face_row16 = WalledNode.create(
+  name: "test",
+  row: 4,
+  col: 20,
+)
+face_row17 = WalledNode.create(
+  name: "test",
+  row: 4,
+  col: 24,
+)
+face_row18 = WalledNode.create(
+  name: "test",
+  row: 4,
+  col: 30,
+)
+face_row19 = WalledNode.create(
+  name: "test",
+  row: 5,
+  col: 14,
+)
+face_row20 = WalledNode.create(
+  name: "test",
+  row: 5,
+  col: 30,
+)
+face_row21 = WalledNode.create(
+  name: "test",
+  row: 6,
+  col: 14,
+)
+face_row22 = WalledNode.create(
+  name: "test",
+  row: 6,
+  col: 30,
+)
+face_row23 = WalledNode.create(
+  name: "test",
+  row: 7,
+  col: 14,
+)
+face_row24 = WalledNode.create(
+  name: "test",
+  row: 7,
+  col: 30,
+)
+face_row25 = WalledNode.create(
+  name: "test",
+  row: 8,
+  col: 14,
+)
+face_row26 = WalledNode.create(
+  name: "test",
+  row: 8,
+  col: 19,
+)
+face_row27 = WalledNode.create(
+  name: "test",
+  row: 8,
+  col: 25,
+)
+face_row28 = WalledNode.create(
+  name: "test",
+  row: 8,
+  col: 30,
+)
+face_row29 = WalledNode.create(
+  name: "test",
+  row: 9,
+  col: 14,
+)
+face_row30 = WalledNode.create(
+  name: "test",
+  row: 9,
+  col: 20,
+)
+face_row31 = WalledNode.create(
+  name: "test",
+  row: 9,
+  col: 24,
+)
+face_row32 = WalledNode.create(
+  name: "test",
+  row: 9,
+  col: 30,
+)
+face_row33 = WalledNode.create(
+  name: "test",
+  row: 10,
+  col: 15,
+)
+face_row34 = WalledNode.create(
+  name: "test",
+  row: 10,
+  col: 21,
+)
+face_row35 = WalledNode.create(
+  name: "test",
+  row: 10,
+  col: 22,
+)
+face_row36 = WalledNode.create(
+  name: "test",
+  row: 10,
+  col: 23,
+)
+face_row37 = WalledNode.create(
+  name: "test",
+  row: 10,
+  col: 29,
+)
+face_row38 = WalledNode.create(
+  name: "test",
+  row: 11,
+  col: 16,
+)
+face_row39 = WalledNode.create(
+  name: "test",
+  row: 11,
+  col: 28,
+)
+face_row40 = WalledNode.create(
+  name: "test",
+  row: 12,
+  col: 17,
+)
+face_row41 = WalledNode.create(
+  name: "test",
+  row: 12,
+  col: 27,
+)
+face_row42 = WalledNode.create(
+  name: "test",
+  row: 13,
+  col: 18,
+)
+face_row43 = WalledNode.create(
+  name: "test",
+  row: 13,
+  col: 19,
+)
+face_row44 = WalledNode.create(
+  name: "test",
+  row: 13,
+  col: 20,
+)
+face_row45 = WalledNode.create(
+  name: "test",
+  row: 13,
+  col: 21,
+)
+face_row46 = WalledNode.create(
+  name: "test",
+  row: 13,
+  col: 22,
+)
+face_row47 = WalledNode.create(
+  name: "test",
+  row: 13,
+  col: 23,
+)
+face_row48 = WalledNode.create(
+  name: "test",
+  row: 13,
+  col: 24,
+)
+face_row49 = WalledNode.create(
+  name: "test",
+  row: 13,
+  col: 25,
+)
+face_row50 = WalledNode.create(
+  name: "test",
+  row: 13,
+  col: 26,
+)
+
+
+
+
+
+
 
