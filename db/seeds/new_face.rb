8500088f87c1319bdb4519cb17e1d5ac986b7e03
Andrew Trac <andrewtrac@Andrews-Air.Home>
2020-07-08 16:34:31 -0400

    New seed for face

---
+++ db/seeds/new_face.rb
@@ -0,0 +1,109 @@
+#setup grid
+face_grid = SavedGrid.create(
+  name: "face"
+)
+
+#create rows
+
+face_row = Row.create(
+  saved_grid_id: 1,   #make sure this grid id is associated with the one above
+  row_num: 0,
+  cols: [18, 19, 20, 21, 22, 23, 24, 25, 26]
+)
+face_row1 = Row.create(
+  saved_grid_id: 1,
+  row_num: 1,
+  cols: [17, 27]
+)
+face_row2 = Row.create(
+  saved_grid_id: 1,   #make sure this grid id is associated with the one above
+  row_num: 2,
+  cols: [16, 28]
+)
+face_row3 = Row.create(
+  saved_grid_id: 1,
+  row_num: 3,
+  cols: [15, 29]
+)
+face_row4 = Row.create(
+  saved_grid_id: 1,   #make sure this grid id is associated with the one above
+  row_num: 4,
+  cols: [14, 20, 24, 30]
+)
+face_row5 = Row.create(
+  saved_grid_id: 1,
+  row_num: 5,
+  cols: [14, 30]
+)
+face_row6 = Row.create(
+  saved_grid_id: 1,   #make sure this grid id is associated with the one above
+  row_num: 6,
+  cols: [14, 30]
+)
+face_row7 = Row.create(
+  saved_grid_id: 1,
+  row_num: 7,
+  cols: [14, 30]
+)
+face_row8 = Row.create(
+  saved_grid_id: 1,   #make sure this grid id is associated with the one above
+  row_num: 8,
+  cols: [14, 19, 25, 30]
+)
+face_row9 = Row.create(
+  saved_grid_id: 1,
+  row_num: 9,
+  cols: [14, 20, 24, 30]
+)
+face_row10 = Row.create(
+  saved_grid_id: 1,
+  row_num: 10,
+  cols: [15, 21, 22, 23, 29]
+)
+face_row11 = Row.create(
+  saved_grid_id: 1,
+  row_num: 11,
+  cols: [16, 28]
+)
+face_row12 = Row.create(
+  saved_grid_id: 1,
+  row_num: 12,
+  cols: [17, 27]
+)
+face_row13 = Row.create(
+  saved_grid_id: 1,
+  row_num: 13,
+  cols: [18, 19, 20, 21, 22, 23, 24, 25, 26]
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
