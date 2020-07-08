3eedf518de38c8e3a411543271c9e673cedc0de8
Andrew Trac <andrewtrac@Andrews-Air.Home>
2020-07-08 18:17:51 -0400

    Added pokemon presaved grid

---
+++ db/seeds/grid3_pokemon.rb
@@ -0,0 +1,80 @@
+
+pokemon_grid = SavedGrid.create(
+  name: "pokemon"
+)
+
+pokemon_row = Row.create(
+  saved_grid_id: 3,
+  row_num: 0,
+  cols: [4, 5, 6, 7, 8, 28, 29]
+)
+pokemon_row1 = Row.create(
+  saved_grid_id: 3,
+  row_num: 1,
+  cols: [3, 9, 25, 26, 27, 28, 29, 30, 31, 32, 33]
+)
+pokemon_row2 = Row.create(
+  saved_grid_id: 3,
+  row_num: 2,
+  cols: [3, 10, 22, 23, 24, 27, 28, 29, 33]
+)
+pokemon_row3 = Row.create(
+  saved_grid_id: 3,
+  row_num: 3,
+  cols: [1, 10, 21, 28, 32]
+)
+pokemon_row4 = Row.create(
+  saved_grid_id: 3,
+  row_num: 4,
+  cols: [0, 5, 6, 7, 8, 9, 10, 11, 20, 27, 28, 31]
+)
+pokemon_row5 = Row.create(
+  saved_grid_id: 3,
+  row_num: 5,
+  cols: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 20, 27, 30]
+)
+pokemon_row6 = Row.create(
+  saved_grid_id: 3,
+  row_num: 6,
+  cols: [2, 4, 6, 9. 10, 11, 19, 22, 28, 30, 31, 32]
+)
+pokemon_row7 = Row.create(
+  saved_grid_id: 3,
+  row_num: 7,
+  cols: [2, 4, 6, 9, 10, 20, 28, 29, 32]
+)
+pokemon_row8 = Row.create(
+  saved_grid_id: 3,
+  row_num: 8,
+  cols: [2, 9, 20, 26, 27, 28, 29, 31]
+)
+pokemon_row9 = Row.create(
+  saved_grid_id: 3,
+  row_num: 9,
+  cols: [3, 7, 8, 10, 20, 29, 32]
+)
+pokemon_row10 = Row.create(
+  saved_grid_id: 3,
+  row_num: 10,
+  cols: [4, 5, 6, 7, 8, 10, 20, 22, 23, 26, 27, 28, 29, 31]
+)
+pokemon_row11 = Row.create(
+  saved_grid_id: 3,
+  row_num: 11,
+  cols: [1, 2, 3, 4, 5, 6, 9,1 0, 20, 21, 28, 30]
+)
+pokemon_row12 = Row.create(
+  saved_grid_id: 3,
+  row_num: 12,
+  cols: [0, 3, 6, 9, 11, 19, 22, 27, 28, 30]
+)
+pokemon_row13 = Row.create(
+  saved_grid_id: 3,
+  row_num: 13,
+  cols: [1, 4, 5, 6, 7, 8, 11, 20, 21, 23, 24, 25, 26, 29]
+)
+pokemon_row14 = Row.create(
+  saved_grid_id: 3,
+  row_num: 14,
+  cols: [2, 3, 4, 9, 10, 27, 28]
+)
