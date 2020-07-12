6ba7defe8cb3848e22f6c406c08aa546c6f775b6
Andrew Trac <andrewtrac@Andrews-Air.Home>
2020-07-11 22:44:52 -0400

    reverted to previous commit

--- db/seeds/grid2_invaders.rb
+++ db/seeds/grid2_invaders.rb
@@ -5,30 +5,40 @@ invaders_grid = SavedGrid.create(
 
 #create rows
 
+invaders_row1 = Row.create(
+  saved_grid_id: 2,
+  row_num: 1,
+  cols: [5, 11, 20, 26, 35, 41]
+)
+invaders_row2 = Row.create(
+  saved_grid_id: 2,   #make sure this grid id is associated with the one above
+  row_num: 2,
+  cols: [6, 10, 21, 25, 36, 40]
+)
 invaders_row3 = Row.create(
   saved_grid_id: 2,
   row_num: 3,
-  cols: [4, 10, 19, 25, 34, 40]
+  cols: [5, 6, 7, 8, 9, 10, 11, 20, 21, 22, 23, 24, 25, 26, 35, 36, 37, 38, 39, 40, 41]
 )
 invaders_row4 = Row.create(
   saved_grid_id: 2,   #make sure this grid id is associated with the one above
   row_num: 4,
-  cols: [5, 9, 20, 24, 35, 39]
+  cols: [4, 5, 7, 8, 9, 11, 12, 19, 20, 22, 23, 24, 26, 27, 34, 35, 37, 38, 39, 41, 42]
 )
 invaders_row5 = Row.create(
   saved_grid_id: 2,
   row_num: 5,
-  cols: [4, 5 , 6, 7, 8, 9, 10,  19, 20, 21, 22, 23, 24, 25, 34, 35, 36, 37, 38, 39, 40]
+  cols: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43]
 )
 invaders_row6 = Row.create(
   saved_grid_id: 2,   #make sure this grid id is associated with the one above
   row_num: 6,
-  cols: [3, 4, 6, 7, 8, 10, 11, 18, 19, 21, 22, 23, 25, 26, 33, 34,36, 37, 38, 40, 41]
+  cols: [3, 5, 6, 7, 8, 9, 10, 11, 13, 18, 20, 21, 22, 23, 24, 25, 26, 28, 33, 35, 36, 37, 38, 39, 40, 41, 43]
 )
 invaders_row7 = Row.create(
   saved_grid_id: 2,
   row_num: 7,
-  cols: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42]
+  cols: [3, 5, 11, 13, 18, 20, 26, 28, 33, 35, 41, 43]
 )
 invaders_row8 = Row.create(
   saved_grid_id: 2,   #make sure this grid id is associated with the one above
